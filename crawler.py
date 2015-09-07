e9164e0e7f8ca5fa99eba2b73dcec9afa040106c
grezbo <keymork@163.com>
2015-09-07 23:04:44 +0800

    fix crawler

--- crawler.py
+++ crawler.py
@@ -2,74 +2,83 @@
 
 __author__ = 'mork'
 
-"""
-this python script is used to catch proxy from www.cnproxy.com
-"""
-
 import urllib2
 from bs4 import BeautifulSoup
-import re
-import proxy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from proxy import Proxy
+import datetime
+import random
+import time
 
 
-def get_urllist():
-    cnproxy_url = 'http://www.cnproxy.com/'
-    request = urllib2.Request(cnproxy_url)
+def get_db_session():
+    engine = create_engine('sqlite:///proxy.db')
+    db_session = sessionmaker(bind=engine)
+    return db_session()
+
+
+def get_raw_page(page_url):
+    time.sleep(random.randint(2, 5))
+    request = urllib2.Request(page_url)
     request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
     response = urllib2.urlopen(request)
-    soup = BeautifulSoup(response, 'lxml', from_encoding='gb18030')
-
-    urllist = []
-    for i in soup.find('div', id='plist').find_all('a'):
-        urllist.append(cnproxy_url+i.get('href'))
-    return urllist
+    return BeautifulSoup(response, 'lxml')
 
 
-def get_proxylist(url):
-    proxylist = []
-    port_map ={'v': '3',
-               'm': '4',
-               'a': '2',
-               'l': '9',
-               'q': '0',
-               'b': '5',
-               'i': '7',
-               'w': '6',
-               'r': '8',
-               'c': '1'}
-    request = urllib2.Request(url)
-    request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
-    response = urllib2.urlopen(request)
-    t = re.compile(r'''<td>(.+?)<SCRIPT type=text/javascript>document.write\(":"\+(.+?)\)</SCRIPT></td><td>(.+?)</td><td>.+?</td><td>(.+?)</td></tr>''')
-    s = response.read()
-    matches = t.findall(s)
-    for match in matches:
-        p = proxy.Proxy(match[0], ''.join(map(lambda x: port_map[x], match[1].split('+'))), match[2], match[3].decode('gb18030').encode('UTF-8'), -1)
-        proxylist.append(p)
-    return proxylist
+def get_urls(type='all'):
+    def get_specific_urls(t):
+        specific_page_list = []
+        specific_url = 'http://www.xicidaili.com/'+t+'/'
+        nn = get_raw_page(specific_url)
+        lastpage_num = nn.find('div', class_='pagination').find_all('a')[-2].string
+        for num in range(1, int(lastpage_num)+1):
+            specific_page_list.append(specific_url+str(num))
+        return specific_page_list
+    url_list = []
+    if type == 'all':
+        url_list.extend(get_specific_urls('nn'))
+        url_list.extend(get_specific_urls('nt'))
+        url_list.extend(get_specific_urls('wn'))
+        url_list.extend(get_specific_urls('wt'))
+        url_list.extend(get_specific_urls('qq'))
+    else:
+        url_list.extend(get_specific_urls(type))
+    return url_list
 
 
-def craw():
-    proxylist = []
-    urllist = get_urllist()
-    for url in urllist:
-        proxylist.extend(get_proxylist(url))
-    return proxylist
+def get_proxies(page_url):
+    print page_url
+    raw_page = get_raw_page(page_url)
+    proxy_list = []
+    for tr in raw_page.find('table', id='ip_list').find_all('tr')[1:]:
+        td = tr.find_all('td')
+        p = Proxy()
+        p.proxy_country = td[1].img.get('alt').upper()
+        p.proxy_ip = td[2].string
+        p.proxy_port = td[3].string
+        p.proxy_location = td[4].text.replace('\n', '').replace(' ', '')
+        p.proxy_type = td[5].string
+        p.connection_type = td[6].string.replace(u'代理', '').upper()
+        p.validation_delay = td[7].find('div', {'class': 'bar'})['title'].replace(u'秒', '')
+        p.connection_delay = td[8].find('div', {'class': 'bar'})['title'].replace(u'秒', '')
+        p.validate_time = datetime.datetime.strptime('20'+td[9].string, '%Y-%m-%d %H:%M')
+        proxy_list.append(p)
+    return proxy_list
+
 
 if __name__ == '__main__':
-    # urllist = get_urllist()
-    # for url in urllist:
-    #     f = file('proxylist.txt', 'a')
-    #     proxylist = get_proxylist(url)
-    #     for i in proxylist:
-    #         line = i['ip'] + ',' + i['port'] + ',' + i['type'] + ',' + i['info'] + '\n'
-    #         f.write(line)
-    #     f.close()
+    session = get_db_session()
+    url_list = get_urls()
+    print 'get urls ready'
+    for url in url_list:
+        proxy_list = get_proxies(url)
+        print 'crawling from %s' % url
+        for proxy in proxy_list:
+            session.add(proxy)
+        session.commit()
+    session.close()
 
-    urllist = get_urllist()
-    proxylist = []
-    for url in urllist:
-        proxylist.extend(get_proxylist(url))
-    for i in proxylist:
-        i.print_proxy()
+
+
 
--- proxy.py
+++ proxy.py
@@ -1,19 +1,42 @@
 __author__ = 'mork'
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Float, DateTime, create_engine
 
-class Proxy(object):
-    def __init__(self, ip, port, type='', describe='', delay=-1):
-        self.ip = ip
-        self.port = port
-        self.type = type
-        self.describe = describe
-        self.delay = delay
+Base = declarative_base()
+
+
+class Proxy(Base):
+
+    __tablename__ = 'proxy'
+
+    proxy_ip = Column(String(20), primary_key=True)
+    proxy_port = Column(String(20))
+    connection_type = Column(String(20))
+    proxy_type = Column(String(20))
+    proxy_country = Column(String(20))
+    proxy_location = Column(String(20))
+    connection_delay = Column(Float)
+    validation_delay = Column(Float)
+    validate_time = Column(DateTime)
+
 
     def print_proxy(self):
-        print '%s:%s %s %s %s' % (self.ip, self.port, self.delay, self.type, self.describe)
+        print ''+self.proxy_ip+self.proxy_port
 
-    def print_proxy_without_delay(self):
-        print '%s:%s %s %s' % (self.ip, self.port, self.type, self.describe)
 
-    def print_proxy_simple(self):
-        print '%s:%s' % (self.ip, self.port)
+
+    #
+    # def __init__(self, ip, port, type, anony='', country='',  location='', connection_delay=-1, validation_delay=-1, validate_time=''):
+    #     self.ip = ip
+    #     self.port = port
+    #     self.type = type
+    #     self.anony = anony
+    #     self.country = country
+    #     self.location = location
+    #     self.connection_delay = connection_delay
+    #     self.validation_delay = validation_delay
+    #     self.validate_time = validate_time
+
+
 
